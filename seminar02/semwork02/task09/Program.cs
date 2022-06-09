df56ecb2a10009bb99c0bb7cdcaf35d128d69820
Fisenko <fisenko-gb@yandex.ru>
2022-06-09 21:30:40 +0400

    task09

--- seminar02/semwork02/task09/Program.cs
+++ seminar02/semwork02/task09/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+
+Clear();
+
+//int number=new Random().Next(10,100);
+int number=79;
+WriteLine($"Случайное число =  {number}");
+
+string answer=number/10>number%10
+?$"Максимальное число = {number/10}"
+:number/10<number%10
+    ?$"Максимальное число = {number%10}"
+    :"Числа равны";
+
+WriteLine(answer);
