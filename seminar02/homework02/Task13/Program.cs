87b593b92169d0b12771276547f06452ef5c8347
Fisenko <fisenko-gb@yandex.ru>
2022-06-10 17:57:28 +0400

    Task10

---
+++ seminar02/homework02/Task13/Program.cs
@@ -0,0 +1,22 @@
+﻿/* **Задача 13:** Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6 */
+using System;
+using static System.Console;
+
+Clear();
+
+int number = new Random().Next(1, 100000);
+WriteLine($"Случайное число =  {number}");
+
+if (number < 100)
+{
+    WriteLine("третьей цифры нет");
+    return;
+}
+
+WriteLine(number.ToString()[2]);
