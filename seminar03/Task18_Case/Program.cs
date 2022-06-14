7ec74b238f3af177d34a7445c14b946fa0374a61
Fisenko <fisenko-gb@yandex.ru>
2022-06-14 19:28:20 +0400

    Task18_Case

---
+++ seminar03/Task18_Case/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите номер четверти: ");
+int x = int.Parse(ReadLine());
+
+string result;
+
+switch (x)
+{
+    case 1: 
+    {
+        result = "x > 0 && y > 0";
+        break;
+    }
+    case 2:
+    {
+        result = "x < 0 && y > 0";
+        break;
+    }
+    case 3:
+    {
+        result = "x < 0 && y < 0";
+        break;
+    }
+    case 4:
+    {
+        result = "x > 0 && y < 0";
+        break;
+    }
+    default: 
+    {
+        result = " не возможно определить, данные введены не корректно...";
+        break;
+    }
+}
+
+WriteLine($" Координаты введенной четверти: {result}");
