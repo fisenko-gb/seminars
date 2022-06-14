924a43497895c8ddcf2016335f8db0c43f7103b2
Fisenko <fisenko-gb@yandex.ru>
2022-06-14 19:09:38 +0400

    task18

---
+++ seminar03/Task18/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите номер четверти: ");
+int x = int.Parse(ReadLine());
+
+string result;
+
+if (x == 1)
+{
+    result = "x > 0 && y > 0";
+}else if (x == 2)
+{
+    result = "x < 0 && y > 0";
+}else if (x == 3)
+{
+    result = "x < 0 && y < 0";
+}else
+{
+    result = "x > 0 && y < 0";
+}
+
+WriteLine($" Координаты введенной четверти: {result}");
