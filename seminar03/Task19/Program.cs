6f15b7a65eaa1059fc4f340d6396a1ed9a5202b7
Fisenko <fisenko-gb@yandex.ru>
2022-06-14 19:50:41 +0400

    Task19 расстояние между точками

---
+++ seminar03/Task19/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите координату Х первой точки: ");
+int x1 = int.Parse(ReadLine());
+Write("Введите координату У первой точки: ");
+int y1 = int.Parse(ReadLine());
+Write("Введите координату Х второй точки: ");
+int x2 = int.Parse(ReadLine());
+Write("Введите координату У второй точки: ");
+int y2 = int.Parse(ReadLine());
+
+int x = x1 - x2;
+int y = y1 - y2;
+
+double rezault = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+
+WriteLine($"Расстояние между точками - {rezault}");
