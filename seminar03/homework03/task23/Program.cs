acf793b14c0f43f5264ed3802f180c05e6134180
Fisenko <fisenko-gb@yandex.ru>
2022-06-14 21:34:03 +0400

    task23

---
+++ seminar03/homework03/task23/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125 */
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите чило: ");
+int x = int.Parse(ReadLine());
+
+for (int i = 1; i <= x; i++)
+{
+    Write($"{Math.Pow(i, 3)}, ");
+}
+
