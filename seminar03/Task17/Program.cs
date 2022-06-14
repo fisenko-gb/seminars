9c9cf5c29fadbdda8b7ad2ed9a3b52285c9729bb
Fisenko <fisenko-gb@yandex.ru>
2022-06-14 18:55:51 +0400

    Seminar03

---
+++ seminar03/Task17/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите Х: ");
+int x = int.Parse(ReadLine());
+Write("Введите Y: ");
+int y = int.Parse(ReadLine());
+int quater = 0;
+
+if (x > 0 && y > 0)
+{
+    quater = 1;
+}else if (x < 0 && y > 0)
+{
+    quater = 2;
+}else if (x < 0 && y < 0)
+{
+    quater = 3;
+}else//if (x > 0 && y < 0)
+{
+    quater = 4;
+}
+
+WriteLine($"{quater} четверть");
