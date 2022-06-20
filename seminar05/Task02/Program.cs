76e50ceda59c02b473c873ab8471fa270536d547
fisenko-gb <fisenko-gb@yandex.ru>
2022-06-20 21:58:10 +0400

    task 2?3

---
+++ seminar05/Task02/Program.cs
@@ -0,0 +1,35 @@
+﻿/* **Задача 32:** Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+
+[-4, -8, 8, 2] -> [4, 8, -8, -2] */
+
+using System;
+using static System.Console;
+Clear();
+
+int[] array = GetRandomArray(5, -5, 5);
+Write($"[{String.Join(", ", array)}] ->");
+GetReverse(array);
+Write($"[{String.Join(", ", array)}]");
+
+int[] GetRandomArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+
+    return result;
+}
+
+void GetReverse(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+       
+            array[i] *= -1;
+        
+    }
+
+}
---
+++ seminar05/Task03/Program.cs
@@ -0,0 +1,42 @@
+﻿/* **Задача 33:** Задайте массив. Напишите программу, которая определяет, присутствует ли заданное 
+число в массиве.
+
+4; массив [6, 7, 19, 345, 3] -> нет
+
+3; массив [6, 7, 19, 345, 3] -> да */
+
+using System;
+using static System.Console;
+Clear();
+
+Write("Ведите число: ");
+int number = int.Parse(ReadLine());
+int[] array = GetRandomArray(5, -2, 5);
+WriteLine($"{number}; массив [{String.Join(", ", array)}] -> {FindNumber(array, number)}");
+
+int[] GetRandomArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+
+    return result;
+}
+
+string FindNumber(int[] array, int numbers)
+{
+    string flag = "нет";
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == numbers)
+        {
+            flag = "да";
+            break;
+        }
+    }
+    return flag;
+}
