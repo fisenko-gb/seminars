5f27145540cc3d64757df4896aaa9f958f5744b7
fisenko-gb <fisenko-gb@yandex.ru>
2022-06-21 21:17:09 +0400

    task34

---
+++ seminar05/homework05/Task34/Program.cs
@@ -0,0 +1,36 @@
+﻿/* Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2 */
+using System;
+using static System.Console;
+Clear();
+
+int[] array = GetRandomArray(25, 100, 999);
+WriteLine($"[{String.Join(", ", array)}] -> {FindNumber(array)}");
+
+int[] GetRandomArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+
+    return result;
+}
+
+int FindNumber(int[] array)
+{
+    int result = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            result += 1;
+        }
+    }
+
+    return result;
+}
