cc57e2a6b4f973c66ba2112cea839516dab5677a
Fisenko <fisenko-gb@yandex.ru>
2022-06-23 23:01:57 +0400

    2

---
+++ seminar06/Task03/Program.cs
@@ -0,0 +1,33 @@
+﻿/* **Задача 44:**Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+
+Если N = 5-> 0 1 1 2 3
+
+Если N = 3 -> 0 1 1
+
+Если N = 7 -> 0 1 1 2 3 5 8 */
+using System;
+using static System.Console;
+
+Clear();
+WriteLine("Введите необходимое количество чисел: ");
+int namber = int.Parse(ReadLine());
+WriteLine($"N = {namber} -> {String.Join(" ", Fib(namber))}");
+
+int[] Fib(int number)
+{
+    int[] array = new int[number];
+    if (number == 1)
+    {
+        array[0] = 0;
+    }
+    else
+    {
+        array[0] = 0;
+        array[1] = 1;
+        for (int i = 2; i < number; i++)
+        {
+            array[i] = array[i - 1] + array[i - 2];
+        }
+    }
+    return array;
+}
---
+++ seminar06/Task04/Program.cs
@@ -0,0 +1,33 @@
+﻿//Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+using System;
+using static System.Console;
+
+Clear();
+
+int[] array = GetRandomArray(4, -5, 5);
+WriteLine($"{String.Join(", ", array)}");
+WriteLine($"{String.Join(", ", GetCopyArray(array))}");
+
+int[] GetRandomArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+
+    return result;
+}
+
+int[] GetCopyArray(int[] array)
+{
+    int[] result = new int[array.Length];
+
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = array[i];
+    }
+
+    return result;
+}
