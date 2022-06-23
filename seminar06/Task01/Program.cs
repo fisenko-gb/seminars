9727a7ae0279fc9bdc62b6bfebcb27f01e40628a
Fisenko <fisenko-gb@yandex.ru>
2022-06-23 22:02:33 +0400

    seminar06

---
+++ seminar06/Task01/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Задача 40: Напишите программу, которая принимает на вход три числа и проверяет,
+может ли существовать треугольник с сторонами такой длины. */
+using System;
+using static System.Console;
+
+Clear();
+WriteLine("Введите первое число: ");
+int namber1 = int.Parse(ReadLine());
+WriteLine("Введите второе число: ");
+int namber2 = int.Parse(ReadLine());
+WriteLine("Введите третье число: ");
+int namber3 = int.Parse(ReadLine());
+
+WriteLine(TreugolnikProverka(namber1, namber2, namber3));
+
+string TreugolnikProverka(int n1, int n2, int n3)
+{
+    if (n1 + n2 > n3 && n1 + n3 > n2 && n2 + n3 > n1)
+    {
+        return "ДА";
+    }
+    else
+    {
+        return "НЕТ";
+    }
+}
+
---
+++ seminar06/Task02/Program.cs
@@ -0,0 +1,27 @@
+﻿/* **Задача 42:**Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+45 -> 101101
+
+3 -> 11
+
+2 -> 10 */
+using System;
+using static System.Console;
+
+Clear();
+WriteLine("Введите число: ");
+int namber = int.Parse(ReadLine());
+
+WriteLine(Preobraz2(namber, 2));
+
+string Preobraz2(int n, int osn)
+{
+    string result = "";
+    while (n >= 1)
+    {
+        result = $"{(n % osn)}" + result;
+        n /= osn;
+    }
+    //result = "1" + result;
+    return result;
+}
