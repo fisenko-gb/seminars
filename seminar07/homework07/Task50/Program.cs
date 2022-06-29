910a25d1ecc3ebbc36b3e2cb8544a9225e0e78cf
fisenko-gb <fisenko-gb@yandex.ru>
2022-06-29 19:44:44 +0400

    Task50

--- seminar07/homework07/Task50/Program.cs
+++ seminar07/homework07/Task50/Program.cs
@@ -9,3 +9,53 @@
 8 4 2 4
 
 1 7 -> такого числа в массиве нет */
+using System;
+using static System.Console;
+
+Clear();
+Write("Введите координаты в массиве через пробел: ");
+string[] sizeS = ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+int rows = int.Parse(sizeS[0]);
+int columns = int.Parse(sizeS[1]);
+
+int[,] ar = GetRandomArray(5, 5);
+PrintArray(ar);
+
+if (rows > ar.GetLength(0) || columns > ar.GetLength(1))
+{
+    Write($"{rows} {columns} -> такого числа в массиве нет");
+}else
+{
+    Write($"{rows} {columns} -> {ar[rows-1, columns-1]}");
+}
+
+
+int[,] GetRandomArray(int row, int columm)
+{
+    int[,] rezult = new int[row, columm];
+
+    for (int i = 0; i < rezult.GetLength(0); i++)
+    {
+        for (int j = 0; j < rezult.GetLength(1); j++)
+        {
+            
+            rezult[i, j] = new Random().Next(1,5);
+            
+        }
+    }
+    return rezult;
+}
+
+void PrintArray(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Write($"{array[i, j]} ");
+        }
+        WriteLine();
+    }
+     WriteLine();
+}
