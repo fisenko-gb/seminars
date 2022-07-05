1916606ebdce58016be7fed02bac33c3ce0b6f7f
fisenko-gb <fisenko-gb@yandex.ru>
2022-07-05 14:06:46 +0400

    Task64

---
+++ seminar09/homework09/Task64/Program.cs
@@ -0,0 +1,24 @@
+﻿//**Задача 64:** Задайте значения M и N. Напишите программу, которая выведет произведение натуральных чисел в промежутке от M до N.
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите число M: ");
+int start = Convert.ToInt32(ReadLine());
+Write("Введите число N: ");
+int stop = Convert.ToInt32(ReadLine());
+
+Write(start < stop ? Multiplication(start, stop) : Multiplication(stop, start));
+
+int Multiplication(int m, int n)
+{
+    
+    if (m > n)
+    {
+        return 1;
+    }
+    
+    return m * Multiplication(m + 1, n);
+   
+}
